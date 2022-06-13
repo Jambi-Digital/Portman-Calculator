c921ecfb694d9f613884ed003a5a59233b0fae45
jambi-rob <rob@jambi.digital>
2022-06-13 11:40:39 +0100

    remove encapsulation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,38 +5,37 @@ require('./lib/nouislider.js');
   * Portman Calculator
   * --------------------------------------------------
   */
+ var PortmanCalculator = PortmanCalculator || {
+  
+    init: function(options) {
 
- ;(function(window, document, undefined) {
+      this.settings(options);       
+      this.build();
 
-    'use strict';
-  
-    var PortmanCalculator = PortmanCalculator || {
-  
-      init: function(options) {
-  
-        this.settings(options);       
-        this.build();
-  
-      },  
-       
-      settings: function(options) {
-  
-        this.config = {
-            utm_source: options.utm_source ? options.utm_source : this.config.utm_source,
-            utm_medium: options.utm_medium ? options.utm_medium : this.config.utm_medium,
-            utm_campaign: options.utm_campaign ? options.utm_campaign : this.config.utm_campaign,
-        };
-  
-      },
-
-      build: function() {
+    },  
      
-      }, 
-  
-    }; 
-  
-    window.PortmanCalculator = PortmanCalculator;
-  
-  })(window, document);
-  
+    settings: function(options) {
+
+      this.config = {
+          utm_source: options.utm_source ? options.utm_source : this.config.utm_source,
+          utm_medium: options.utm_medium ? options.utm_medium : this.config.utm_medium,
+          utm_campaign: options.utm_campaign ? options.utm_campaign : this.config.utm_campaign,
+      };
+
+    },
+
+    build: function() {
+   
+    }, 
+
+  }; 
+
+  window.PortmanCalculator = PortmanCalculator;
+  if (typeof Object.create !== 'function') {
+    Object.create = function(obj) {
+      function F() {}
+      F.prototype = obj;
+      return new F();
+    };
+  }
   
